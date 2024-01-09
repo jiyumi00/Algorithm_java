ae40655df85a52098b1eadda1288bde71da447d0
jiyumi <jiyumi00@naver.com>
2024-01-09 15:16:13 +0900

    1. 문자찾기

--- Java 강의/1. String/FindWord_01.java
+++ Java 강의/1. String/FindWord_01.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
+import java.util.*;
 public class FindWord_01{
+	public static int solution(String word,char letter){
+		int cnt=0;
+		word=word.toUpperCase();
+		letter=Character.toUpperCase(letter);
+		for(int i=0;i<word.length();i++){
+			if(word.charAt(i)==letter) cnt++;
+		}
+		return cnt;
+	}
 	public static void main(String[]args){
 		Scanner scanner=new Scanner(System.in);
-		String word,str;
+		String word;
 		char letter;
-		int cnt=0;
 		
 		word=scanner.next();
-		str=scanner.next();
-		letter=str.charAt(0);
-		
-		for(int i=0;i<word.length();i++){
-			if(word.charAt(i)==letter){
-				cnt++;
-			}	
-			if((int)(word.charAt(i))+32==(int)letter){
-				cnt++;
-			}
-			if((int)(word.charAt(i))-32==(int)letter){
-				cnt++;
-			}
-		}
-		System.out.print(cnt);
+		letter=scanner.next().charAt(0);
+	
+		System.out.print(solution(word,letter));
 	}
 }
