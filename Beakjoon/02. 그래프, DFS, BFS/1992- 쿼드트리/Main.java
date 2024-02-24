f6fae76749f838237409b7b363ee8eac770d0003
jiyumi <jiyumi00@naver.com>
2024-02-24 09:58:42 +0900

    #재귀함수 #분할정복

---
+++ Beakjoon/02. 그래프, DFS, BFS/1992- 쿼드트리/Main.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	
+	static int map[][];
+	static int n;
+	static String str;
+	
+	static String quadTree(int y,int x,int size){
+		//size가 1일때 
+		if(size==1){ 
+			return Integer.toString(map[y][x]);
+		}
+		int num=map[y][x]; //첫번 째 시작점
+		str="";
+		//0과 1 혼합
+		for(int i=y;i<y+size;i++){
+			for(int j=x;j<x+size;j++){
+				if(num!=map[i][j]){
+					str+="(";
+					str+=quadTree(y,x,size/2);
+					str+=quadTree(y,x+size/2,size/2);
+					str+=quadTree(y+size/2,x,size/2);
+					str+=quadTree(y+size/2,x+size/2,size/2);
+					str+=")";
+					return str;
+				}
+			}
+		}
+		//모두 0 또는 모두 1일때
+		return Integer.toString(map[y][x]);
+	}
+	
+
+	public static void main(String[]args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	
+		n=Integer.parseInt(br.readLine());
+		
+		map=new int[n][n];
+		for(int i=0;i<n;i++){
+			String s=br.readLine();
+			for(int j=0;j<n;j++){
+				map[i][j]=s.charAt(j)-'0';
+			}
+		}
+		System.out.println(quadTree(0,0,n));
+	}
+}
