6a34da8fec8f7e1cd19ba487fcb6dc6c86240a0c
jiyumi <jiyumi00@naver.com>
2024-03-05 10:23:21 +0900

    #구현 #문자열

--- Beakjoon/02. 그래프, DFS, BFS/2852- NBA 농구/Main.java
+++ Beakjoon/02. 그래프, DFS, BFS/2852- NBA 농구/Main.java
@@ -3,11 +3,16 @@ import java.io.*;
 import java.math.*;
 
 public class Main{
+	static String changeTime(int num){
+		String m = "00" + (num / 60);
+        String s = "00" + (num % 60);
+        return m.substring(m.length() - 2) + ":" + s.substring(s.length() - 2);
+	}
 	public static void main(String[]args) throws IOException{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		int n,teamNum,minute,second,teamTime;
-		String teamTime;
+		int n,teamNum,minute,second,lastTime=0,totalTime=48*60;
+		int oneScore=0,twoScore=0,oneTime=0,twoTime=0;
 		
 		n=Integer.parseInt(br.readLine());
 		
@@ -20,7 +25,32 @@ public class Main{
 			minute=Integer.parseInt(st.nextToken());
 			second=Integer.parseInt(st.nextToken());
 			
-			teamTime=minute*60+second;
+			int currentTime=minute*60+second;
+			
+			if(oneScore>twoScore){
+				oneTime+=currentTime-lastTime;
+			}
+			else if(twoScore>oneScore){
+				twoTime+=currentTime-lastTime;
+			}
+			
+			lastTime=currentTime;
+			
+			if(teamNum==1){
+				oneScore++;
+			}
+			else{
+				twoScore++;
+			}
 		}
+		if(oneScore>twoScore){
+			oneTime+=totalTime-lastTime;
+		}
+		else if(twoScore>oneScore){
+			twoTime+=totalTime-lastTime;
+		}
+		System.out.println(changeTime(oneTime));
+		System.out.print(changeTime(twoTime));
+			
 	}
 }
